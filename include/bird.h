8bed7701860cd29b79c93d2b211c7a346cca2d52
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-04-18 15:22:41 -0400

    Created files

---
+++ include/bird.h
@@ -0,0 +1,12 @@
+#ifndef FINAL_PROJECT_BIRD_H
+#define FINAL_PROJECT_BIRD_H
+
+namespace flappybird {
+    class bird {
+    public:
+        
+    private:
+        
+    };
+}
+#endif //FINAL_PROJECT_BIRD_H
