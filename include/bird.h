94cd35674f74082f9cf6a64dfe20ab383d30eb29
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-04-18 15:40:05 -0400

    Created files

--- include/bird.h
+++ include/bird.h
@@ -1,12 +1,19 @@
 #ifndef FINAL_PROJECT_BIRD_H
 #define FINAL_PROJECT_BIRD_H
 
+#include <vector>
+#include "cinder/gl/gl.h"
+
 namespace flappybird {
-    class bird {
+    class Bird {
     public:
+        Bird() = default;
+        
+        Bird(const glm::vec2& position, const glm::vec2& velocity);
         
     private:
-        
+        glm::vec2 position_;
+        glm::vec2 velocity_;
     };
 }
 #endif //FINAL_PROJECT_BIRD_H
