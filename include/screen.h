a1512368765e9455daa0dd0b9abd7557f831ac1a
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-04-21 00:40:49 -0400

    Implemented methods

--- include/screen.h
+++ include/screen.h
@@ -1,10 +1,48 @@
 #ifndef FINAL_PROJECT_SCREEN_H
 #define FINAL_PROJECT_SCREEN_H
 
+#include "barrel.h"
+#include "bird.h"
+#include "cinder/gl/gl.h"
+#include <vector>
+
 namespace flappybird {
+    /**
+     * Class for screen
+     */
     class Screen {
     public:
+        /**
+         * Explicit constructor
+         * @param top_left vec2 for top left pixel
+         * @param bottom_right vec2 for bottom right pixel
+         */
+        Screen(const glm::vec2 &top_left, const glm::vec2 &bottom_right);
+        
+        /**
+         * Displays screen
+         */
+        void Display();
+
+        /**
+         * Updates position and velocity of bird
+         */
+        void AdvanceOneFrame();
+
+        /**
+         * Getter for Bird
+         * @return Bird& for bird in screen
+         */
+        Bird &GetBird();
+
     private:
+        glm::vec2 top_left_;
+        glm::vec2 bottom_right_;
+
+        glm::vec2 initial_position = glm::vec2(10, 535);
+        glm::vec2 initial_velocity = glm::vec2(1.5, 0);
+        Bird bird_ = Bird(initial_position, initial_velocity);
+        std::vector<Barrel> barrels_;
     };
-}
-#endif //FINAL_PROJECT_SCREEN_H
+}// namespace flappybird
+#endif//FINAL_PROJECT_SCREEN_H
--- src/screen.cpp
+++ src/screen.cpp
@@ -1,6 +1,25 @@
 #include "screen.h"
 
 namespace flappybird {
-    
+
+    Screen::Screen(const glm::vec2 &top_left, const glm::vec2 &bottom_right) {
+        top_left_ = top_left;
+        bottom_right_ = bottom_right;
+    }
+
+    void Screen::Display() {
+
+    }
+
+    void Screen::AdvanceOneFrame() {
+        if (bird_.IsOnGround()) {
+            bird_.StopMoving();
+        }
+        bird_.UpdatePosition();
+    }
+
+    Bird &Screen::GetBird() {
+        return bird_;
+    }
 }
 
