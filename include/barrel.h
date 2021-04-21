d01f48f3324af159fde21442ed3ebd6a36505578
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-04-21 00:39:48 -0400

    Implemented methods

--- include/barrel.h
+++ include/barrel.h
@@ -1,4 +1,27 @@
 #ifndef FINAL_PROJECT_BARREL_H
 #define FINAL_PROJECT_BARREL_H
 
+#include "cinder/gl/gl.h"
+
+namespace flappybird {
+    /**
+     * Class for green barrels
+     */
+    class Barrel {
+    public:
+        Barrel() = default;
+        
+        /**
+         * Explicit constructor
+         * @param position vec2 for position
+         * @param velocity vec2 for velocity
+         */
+        Barrel(const glm::vec2& position, const glm::vec2& velocity);
+        
+    private:
+        const size_t kWidth = 30;
+        glm::vec2 position_;
+        glm::vec2 velocity_;
+    };
+}
 #endif //FINAL_PROJECT_BARREL_H
--- src/barrel.cpp
+++ src/barrel.cpp
@@ -1,2 +1,9 @@
 #include "barrel.h"
 
+namespace flappybird {
+
+    Barrel::Barrel(const glm::vec2 &position, const glm::vec2 &velocity) {
+        position_ = position;
+        velocity_ = velocity;
+    }
+}
