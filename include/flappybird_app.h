75f44ca531950421b08b8e60ada40730183e8aa9
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-04-21 00:41:09 -0400

    Implemented methods

--- include/flappybird_app.h
+++ include/flappybird_app.h
@@ -1,4 +1,37 @@
 #ifndef FINAL_PROJECT_FLAPPYBIRD_APP_H
 #define FINAL_PROJECT_FLAPPYBIRD_APP_H
 
-#endif //FINAL_PROJECT_FLAPPYBIRD_APP_H
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "screen.h"
+
+namespace flappybird {
+    /**
+     * Game Engine
+     */
+    class FlappyBirdApp : public ci::app::App {
+    public:
+        FlappyBirdApp();
+
+        /**
+         * Implements Display function of Screen
+         */
+        void draw() override;
+
+        /**
+         * Implements AdvanceOneFrame function of screen
+         */
+        void update() override;
+
+        /**
+         * When space bar is pressed, moves bird up by 20 pixels
+         * @param event KeyEvent representing space bar
+         */
+        void keyDown(ci::app::KeyEvent event) override;
+
+    private:
+        const size_t kWindowSize = 1070;
+        Screen screen_;
+    };
+}// namespace flappybird
+#endif//FINAL_PROJECT_FLAPPYBIRD_APP_H
--- src/flappybird_app.cpp
+++ src/flappybird_app.cpp
@@ -1,2 +1,25 @@
 #include "flappybird_app.h"
 
+using glm::vec2;
+
+namespace flappybird {
+
+    FlappyBirdApp::FlappyBirdApp() : screen_(Screen(vec2(0, 0), 
+                                                    vec2(kWindowSize, kWindowSize))) {
+        ci::app::setWindowSize(kWindowSize, kWindowSize);
+    }
+
+    void FlappyBirdApp::draw() {
+        screen_.Display();
+    }
+
+    void FlappyBirdApp::update() {
+        screen_.AdvanceOneFrame();
+    }
+
+    void FlappyBirdApp::keyDown(ci::app::KeyEvent event) {
+        if (event.getCode() == ci::app::KeyEvent::KEY_SPACE) {
+            screen_.GetBird().GetPosition().y += 20;
+        }
+    }
+}
