643fc645515b309e14f64cb714b5c06aa5a4f7fd
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-04-21 00:40:14 -0400

    Implemented methods

--- include/bird.h
+++ include/bird.h
@@ -1,23 +1,62 @@
 #ifndef FINAL_PROJECT_BIRD_H
 #define FINAL_PROJECT_BIRD_H
 
-#include <vector>
 #include "cinder/gl/gl.h"
+#include <vector>
 
 namespace flappybird {
+    /**
+     * Class for Bird
+     */
     class Bird {
     public:
         Bird() = default;
-        
-        Bird(const glm::vec2& position, const glm::vec2& velocity);
-        
-        const glm::vec2& GetPosition() const;
-        
-        const glm::vec2& GetVelocity() const;
-        
+
+        /**
+         * Explicit constructor
+         * @param position vec2 for position
+         * @param velocity vec2 for velocity
+         */
+        Bird(const glm::vec2 &position, const glm::vec2 &velocity);
+
+        /**
+         * Getter for position
+         * @return vec2& for position
+         */
+        glm::vec2 &GetPosition();
+   
+        /**
+         * Getter for velocity
+         * @return vec2& for velocity
+         */
+        const glm::vec2 &GetVelocity() const;
+
+        /**
+         * Adjusts position based on velocity vector
+         */
+        void UpdatePosition();
+
+        /**
+         * Adjusts y-velocity based on gravitational acceleration
+         */
+        void UpdateVelocity();
+
+        /**
+         * Sets velocity to 0
+         */
+        void StopMoving();
+
+        /**
+         * Checks if bird in on the ground
+         * @return bool if bird on ground
+         */
+        const bool IsOnGround() const;
+
     private:
         glm::vec2 position_;
         glm::vec2 velocity_;
+
+        const float acceleration = 1.5f;
     };
-}
-#endif //FINAL_PROJECT_BIRD_H
+}// namespace flappybird
+#endif//FINAL_PROJECT_BIRD_H
--- src/bird.cpp
+++ src/bird.cpp
@@ -5,5 +5,30 @@ namespace flappybird {
         position_ = position;
         velocity_ = velocity;
     }
-}
 
+    glm::vec2 &Bird::GetPosition() {
+        return position_;
+    }
+
+    const glm::vec2 &Bird::GetVelocity() const {
+        return velocity_;
+    }
+
+    void Bird::UpdatePosition() {
+        UpdateVelocity();
+        position_ += velocity_;
+    }
+
+    void Bird::StopMoving() {
+        velocity_.x = 0;
+        velocity_.y = 0;
+    }
+
+    const bool Bird::IsOnGround() const {
+        return position_.y == 0;
+    }
+
+    void Bird::UpdateVelocity() {
+        velocity_.y -= acceleration;
+    }
+}// namespace flappybird
