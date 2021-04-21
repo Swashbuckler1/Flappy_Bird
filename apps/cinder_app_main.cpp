a279605c3f8386a392501d31eab71d7d1813667d
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-04-21 00:40:30 -0400

    Added macros

--- apps/cinder_app_main.cpp
+++ apps/cinder_app_main.cpp
@@ -1,2 +1,10 @@
+#include "flappybird_app.h"
 
+using flappybird::FlappyBirdApp;
+
+void prepareSettings(FlappyBirdApp::Settings* settings) {
+    settings -> setResizable(false);
+}
+
+CINDER_APP(FlappyBirdApp, ci::app::RendererGl, prepareSettings);
 
