692784a4a79d4bd82b7470ba3f80fc5c8532103b
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-04-28 00:55:58 -0400

    Implemented barrel code

--- include/barrel.h
+++ include/barrel.h
@@ -10,18 +10,26 @@ namespace flappybird {
     class Barrel {
     public:
         Barrel() = default;
-        
+
         /**
          * Explicit constructor
          * @param position vec2 for position
          * @param velocity vec2 for velocity
          */
-        Barrel(const glm::vec2& position, const glm::vec2& velocity);
+        Barrel(const glm::vec2 &top_left, const glm::vec2 &bottom_right);
         
+        glm::vec2 GetTopLeft();
+        
+        glm::vec2 GetBottomRight();
+        
+        const ci::Color GetColor() const;
+
     private:
-        const size_t kWidth = 30;
-        glm::vec2 position_;
+        glm::vec2 top_left_;
+        glm::vec2 bottom_right_;
         glm::vec2 velocity_;
+
+        ci::Color color_;
     };
-}
-#endif //FINAL_PROJECT_BARREL_H
+}// namespace flappybird
+#endif//FINAL_PROJECT_BARREL_H
--- src/barrel.cpp
+++ src/barrel.cpp
@@ -2,8 +2,22 @@
 
 namespace flappybird {
 
-    Barrel::Barrel(const glm::vec2 &position, const glm::vec2 &velocity) {
-        position_ = position;
-        velocity_ = velocity;
+
+    Barrel::Barrel(const glm::vec2 &top_left, const glm::vec2 &bottom_right) {
+        top_left_ = top_left;
+        bottom_right_ = bottom_right;
+        color_ = ci::Color(0, 1,0);
+    }
+    
+    glm::vec2 Barrel::GetTopLeft() {
+        return top_left_;
+    }
+    
+    glm::vec2 Barrel::GetBottomRight() {
+        return bottom_right_;
+    }
+    
+    const ci::Color Barrel::GetColor() const {
+        return color_;
     }
 }
