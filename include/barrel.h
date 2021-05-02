32662122d9c3a5a7a8d9c52c4f9ae17dfd30a4bc
Suhaas Narayan <40607921+Swashbuckler1@users.noreply.github.com>
2021-05-02 15:52:37 -0400

    Commit before checkout

--- include/barrel.h
+++ include/barrel.h
@@ -17,21 +17,23 @@ namespace flappybird {
          * @param position vec2 for position
          * @param velocity vec2 for velocity
          */
-        Barrel(const glm::vec2 &top_left, const glm::vec2 &bottom_right);
-        
+        Barrel(const glm::vec2 &top_left, const glm::vec2 &bottom_right, const glm::vec2 &velocity);
+
         glm::vec2 GetTopLeft();
-        
+
         glm::vec2 GetBottomRight();
-        
+
         const ci::Color GetColor() const;
-        
+
         bool HasBirdHit(Bird& bird);
-        
+
         bool HasBirdHitHorizontal(Bird& bird);
-        
+
         bool BarrelWidthContainsBird(Bird& bird);
-        
+
         void HandleBirdCollision(Bird& bird);
+
+        void UpdatePosition();
 
     private:
         glm::vec2 top_left_;
